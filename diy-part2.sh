4aa0ac54e9c86a6e4bfb628cb8f9f67baf30198d
TaTaiLa <167389989+TaTaiLa@users.noreply.github.com>
2024-11-17 16:47:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,18 +18,3 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
-# git clone https://github.com/Zxilly/UA2F.git package/UA2F
-git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
-
-
-git clone https://github.com/MrTaiKe/Action_OpenWrt_Xiaomi_R4AGv2.git ./replace
-cp ./replace/LEDE/hw2add/476-mtd-spi-nor-add-eon-en25q128.patch  ./target/linux/generic/pending-5.10/476-mtd-spi-nor-add-eon-en25q128.patch
-cp ./replace/LEDE/hw2add/mt7621_xiaomi_mi-router-4a-gigabit-v2.dts  ./target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit-v2.dts
-cp ./replace/LEDE/hw2add/mt7621.mk  ./target/linux/ramips/image/mt7621.mk
-cp ./replace/LEDE/hw2add/01_leds  ./target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-cp ./replace/LEDE/hw2add/02_network  ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-echo "copy ok"
-rm -rf ./replace
-echo "del ok"
